fc7f0035cd36cba18fccf3755ccf002fd1d11e93
MeralSavas <meralsavas@gmail.com>
2023-02-04 00:40:43 +0000

    added details

--- rc04_tour-places/solution/src/components/Main/Card.jsx
+++ rc04_tour-places/solution/src/components/Main/Card.jsx
@@ -1,21 +1,18 @@
-
-
 const Card = (data) => {
-    console.log("ne geliyo",data);
-    const {title, image, desc} = data;
-    return(
-        <div className="cards">
-            <div className="title">
-                <h1>{title}</h1>
-            </div>
-            <img src={image} alt="" />
-            <div className="card-over">
-                <p>
-                    {desc}
-                </p>
-            </div>
-        </div>
-    )
-}
+  console.log("ne geliyo", data);
+  const { title, image, desc } = data;
+  return (
+    <div className="cards">
+      <div className="title">
+        <h1>{title}</h1>
+        {/* <h1>{data.title}</h1> */}
+      </div>
+      <img src={image} alt="" />
+      <div className="card-over">
+        <p>{desc}</p>
+      </div>
+    </div>
+  );
+};
 
 export default Card;
