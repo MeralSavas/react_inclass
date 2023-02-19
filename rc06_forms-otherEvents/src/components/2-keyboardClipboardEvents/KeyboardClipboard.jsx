66ed7e5ae4045b514319d2ae8d8e56d23b37cd66
MeralSavas <meralsavas@gmail.com>
2023-02-19 23:59:06 +0000

    clipboard

--- rc06_forms-otherEvents/src/components/2-keyboardClipboardEvents/KeyboardClipboard.jsx
+++ rc06_forms-otherEvents/src/components/2-keyboardClipboardEvents/KeyboardClipboard.jsx
@@ -2,12 +2,15 @@ import { useState } from "react";
 
 const KeyboardClipboard = () => {
   const [inputValue, setInputValue] = useState("");
+  //  
 
   const handleKeyDown = (e) => {
+    // bu kisim inputa rakam girilmesini engellemek icin
     console.log(e.keyCode);
     if (e.keyCode >= 48 && e.keyCode <= 57) {
       alert("Please dont enter a number");
       e.preventDefault();
+      // bu kisim girdiyi iptal ediyoruz. Yazma yok
     }
     e.keyCode === 13 && setInputValue("");
   };
@@ -15,6 +18,7 @@ const KeyboardClipboard = () => {
   const handleAreaPaste = (e) => {
     e.target.style.border = "2px solid red";
     e.target.value += e.clipboardData.getData("text").toLocaleUpperCase();
+    // burada buyuk harf kullanimini gosterdi toLocalUpperCase
     e.preventDefault();
   };
 
@@ -30,6 +34,7 @@ const KeyboardClipboard = () => {
           setInputValue(e.target.value);
         }}
         onKeyDown={handleKeyDown}
+        // rakam girilmesini engellemek icin
       />
 
       <p
@@ -48,6 +53,7 @@ const KeyboardClipboard = () => {
         id="area"
         rows="10"
         onPaste={handleAreaPaste}
+        // input icine bir seyler yapistirma
       ></textarea>
     </div>
   );
