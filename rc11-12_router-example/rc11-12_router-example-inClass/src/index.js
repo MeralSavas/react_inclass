2af8652c9b736e86767bd0e7f0e964dc7aa3274a
MeralSavas <meralsavas@gmail.com>
2023-03-09 00:53:14 +0000

    comment

--- rc11-12_router-example/rc11-12_router-example-inClass/src/index.js
+++ rc11-12_router-example/rc11-12_router-example-inClass/src/index.js
@@ -10,3 +10,5 @@ root.render(
     <App />
   </BrowserRouter>
 );
+
+// normalde App.js te browser routeri sarmallayabilirdik ama burada da olabilir. app i burada sarmmaldik parentlerden birini sarmallamk gerekiyordu
