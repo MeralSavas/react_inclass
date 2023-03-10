38bff3d8c12658dc2b6dbb3ff225b3d9d43ee270
MeralSavas <meralsavas@gmail.com>
2023-03-10 03:48:17 +0000

    private router

--- rc11-12_router-example/rc11-12_router-example-inClass/src/pages/Paths.jsx
+++ rc11-12_router-example/rc11-12_router-example-inClass/src/pages/Paths.jsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate, Outlet } from "react-router-dom";
+ import { Link, useNavigate, Outlet } from "react-router-dom";
 
 const Paths = () => {
   const navigate = useNavigate();
