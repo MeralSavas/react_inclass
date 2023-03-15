94dadc41065d8dd834901e28cb8aad6fda0da4b1
MeralSavas <meralsavas@gmail.com>
2023-03-15 23:46:13 +0000

    committed

--- rc14-15_recipe-app/recipe-app-solution/src/pages/login/Login.style.jsx
+++ rc14-15_recipe-app/recipe-app-solution/src/pages/login/Login.style.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import Flex from '../../components/globalStyles/Flex.style';
 
 
- export const LoginContainer= styled(Flex)`
+export const LoginContainer= styled(Flex)`
   height: 100vh;
   background-image: url('https://picsum.photos/1600/900');
   background-repeat: no-repeat;
